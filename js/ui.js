7cbbddce6aafdcea4a5db174451fddd996ae0377
CodeXanishh <mrunknown101099@gmail.com>
2026-06-26 21:10:49 +0530

    Add responsive navigation bar

---
+++ js/ui.js
@@ -0,0 +1,16 @@
+
+const navbar=document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>30){
+
+navbar.classList.add("scrolled");
+
+}else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
